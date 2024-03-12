0a2898805036bc6f4faefd61b3d250d2bb639c0e
boram065 <w2216@gmail.com>
2024-03-12 20:46:29 +0900

    번역 text, box 추가

---
+++ src/Translation.js
@@ -0,0 +1,14 @@
+import './Translation.css';
+
+function Translation() {
+    return (
+      <span className="container">
+        <div className="textDiv"><h1>번역</h1></div>
+        <span className="boxContainer">
+            <div className="box"></div>
+        </span>
+      </span>
+    );
+}
+
+export default Translation;
