13c95b51167ebae23bfcc7fce3758d5c689b427c
boram065 <w2216@e-mirim.hs.kr>
2024-03-14 20:55:51 +0900

    배경화면(날먹용) 개발 시작

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import Translation from './Translation';
+import Desktop from './Desktop-screen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Desktop />,
     <Translation />
 );
