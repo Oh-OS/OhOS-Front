23db9fb0ef1b474e2e87e78b6f7497acf1dc67f5
boram065 <w2216@e-mirim.hs.kr>
2024-03-16 10:58:21 +0900

    아이콘에 마우스 hover 시 아이콘 확대/축소

--- src/Desktop-screen.js
+++ src/Desktop-screen.js
@@ -1,4 +1,5 @@
 import './Desktop-screen.css';
+import React, { useState } from 'react';
 
 function Desktop() {
     const style = {
@@ -49,18 +50,26 @@ function Desktop() {
         alignItems: "center"
     }
 
+    
     const iconsStyle = {
         position: style.position.relative,
-        width: "65px",
-        height: "65px",
+        width: "50px",
+        height: "50px",
+        transition: "transform 0.3s ease" // 변환 효과를 부드럽게
     }
-
+    
     const iconsImgStyle = {
         position: style.position.absolute,
         maxWidth: "100%",
         maxHeight: "100%",
         zIndex: "100"
     }
+    
+    // 아이콘 확대/축소를 위한 상태 변수 및 함수
+    const [hoveredIcon, setHoveredIcon] = useState(null);
+    const handleIconHover = (icon) => { setHoveredIcon(icon); };
+    const handleIconLeave = () => { setHoveredIcon(null); };
+
 
     return (
         <div style={topStyle}>
@@ -69,13 +78,13 @@ function Desktop() {
             </div>
             <div style={containerStyle}>
                 <div style={iconContainerStyle}>
-                    <div style={iconsStyle}> <img src={process.env.PUBLIC_URL + '../images/PhotoboothIcon.png'} alt="photobooth" style={iconsImgStyle} /> </div>
-                    <div style={iconsStyle}> <img src={process.env.PUBLIC_URL + '../images/MapIcon.png'} alt="map" style={iconsImgStyle} /> </div>
-                    <div style={iconsStyle}> <img src={process.env.PUBLIC_URL + '../images/WeatherIcon.png'} alt="weather" style={iconsImgStyle} /> </div>
-                    <div style={iconsStyle}> <img src={process.env.PUBLIC_URL + '../images/LineIcon.png'} alt="line" style={iconsImgStyle} /> </div>
-                    <div style={iconsStyle}> <img src={process.env.PUBLIC_URL + '../images/VscodeIcon.png'} alt="vscode" style={iconsImgStyle} /> </div>
-                    <div style={iconsStyle}> <img src={process.env.PUBLIC_URL + '../images/SiriIcon.png'} alt="siri" style={iconsImgStyle} /> </div>
-                    <div style={iconsStyle}> <img src={process.env.PUBLIC_URL + '../images/SafariIcon.png'} alt="safari" style={iconsImgStyle} /> </div>
+                <div style={{...iconsStyle, transform: hoveredIcon === 'Photobooth' ? "scale(1.2)" : "scale(1)"}} onMouseEnter={() => handleIconHover('Photobooth')} onMouseLeave={handleIconLeave}> <img src={process.env.PUBLIC_URL + '../images/PhotoboothIcon.png'} alt="photobooth" style={iconsImgStyle} /> </div>
+                    <div style={{...iconsStyle, transform: hoveredIcon === 'Map' ? "scale(1.2)" : "scale(1)"}} onMouseEnter={() => handleIconHover('Map')} onMouseLeave={handleIconLeave}> <img src={process.env.PUBLIC_URL + '../images/MapIcon.png'} alt="map" style={iconsImgStyle} /> </div>
+                    <div style={{...iconsStyle, transform: hoveredIcon === 'Weather' ? "scale(1.2)" : "scale(1)"}} onMouseEnter={() => handleIconHover('Weather')} onMouseLeave={handleIconLeave}> <img src={process.env.PUBLIC_URL + '../images/WeatherIcon.png'} alt="weather" style={iconsImgStyle} /> </div>
+                    <div style={{...iconsStyle, transform: hoveredIcon === 'Line' ? "scale(1.2)" : "scale(1)"}} onMouseEnter={() => handleIconHover('Line')} onMouseLeave={handleIconLeave}> <img src={process.env.PUBLIC_URL + '../images/LineIcon.png'} alt="line" style={iconsImgStyle} /> </div>
+                    <div style={{...iconsStyle, transform: hoveredIcon === 'Siri' ? "scale(1.2)" : "scale(1)"}} onMouseEnter={() => handleIconHover('Siri')} onMouseLeave={handleIconLeave}> <img src={process.env.PUBLIC_URL + '../images/SiriIcon.png'} alt="siri" style={iconsImgStyle} /> </div>
+                    <div style={{...iconsStyle, transform: hoveredIcon === 'Vscode' ? "scale(1.2)" : "scale(1)"}} onMouseEnter={() => handleIconHover('Vscode')} onMouseLeave={handleIconLeave}> <img src={process.env.PUBLIC_URL + '../images/VscodeIcon.png'} alt="vscode" style={iconsImgStyle} /> </div>
+                    <div style={{...iconsStyle, transform: hoveredIcon === 'Safari' ? "scale(1.2)" : "scale(1)"}} onMouseEnter={() => handleIconHover('Safari')} onMouseLeave={handleIconLeave}> <img src={process.env.PUBLIC_URL + '../images/SafariIcon.png'} alt="safari" style={iconsImgStyle} /> </div>
                 </div>
             </div>
         </div>
