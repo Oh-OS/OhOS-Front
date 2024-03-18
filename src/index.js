af09308b322d941b182504225bfc257704326a6c
boram065 <boram4784@gmail.com>
2024-03-18 21:04:14 +0900

    배경화면 로컬로 띄우기

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Translation from './Translation';
 import Desktop from './Desktop-screen';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Desktop />,
-    // <Translation />
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Desktop />} />
+            <Route path="/translation" element={<Translation />} />
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
