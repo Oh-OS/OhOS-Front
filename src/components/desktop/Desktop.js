186ec59ee7bddb60252d83e606c45581d23050ef
boram065 <boram4784@gmail.com>
2024-03-22 08:07:26 +0900

    desktop.css, dock.css 코드 수정

--- src/components/desktop/Desktop.js
+++ src/components/desktop/Desktop.js
@@ -3,14 +3,18 @@ import Dock from '../dock/Dock';
 
 function Desktop() {
     return (
-        <div className={styles['topStyle']}>
-            <div className={styles['backgroundStyle']}>
-                <img src={process.env.PUBLIC_URL + '/images/background.png'} className={styles['backgroundImgStyle']} />
+        <>
+            <div className={styles['style']}>
+                <div className={styles['topStyle']}>
+                    <div className={styles['backgroundStyle']}>
+                        <img src={process.env.PUBLIC_URL + '/images/background.png'} className={styles['backgroundImgStyle']} />
+                    </div>
+                    <div className={styles['containerStyle']}>
+                        <Dock />
+                    </div>
+                </div>
             </div>
-            <div className={styles['containerStyle']}>
-                <Dock />
-            </div>
-        </div>
+        </>
     )
 }
 
