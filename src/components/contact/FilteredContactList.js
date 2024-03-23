11b22a195f807eac862ab440704e8cdf3c839209
seohyunCho <shanny06@naver.com>
2024-03-23 19:42:03 +0900

    필터링된 연락처에서 연락처 선택

--- src/components/contact/FilteredContactList.js
+++ src/components/contact/FilteredContactList.js
@@ -1,20 +1,36 @@
+import { useState, useEffect, useContext } from 'react';
 import '../../styles/common/Style.css';
 import styles from '../../styles/contact/FilteredContactList.module.css'
+import { ContactContext } from './ContactProvider';
 
-const showContactList = filteredContactList => {
-    let contactList = filteredContactList.map((contact, index) => {
-        return <div className={styles['contact']} key={index}>{contact}</div>
-    })
+const userNameList = ['김하은', '양가윤', '이서영', '이해원', '조서현', '최보람', '황혜경'];
 
-    return contactList;
-}
+function FilteredContactList({ inputValue }) {
+    const [ filteredContacts, setFilteredContacts ] = useState([]);
+    const { selectedName, setSelectedName } = useContext(ContactContext);
 
-function FilteredContactList({ filteredContactList }) {
+    useEffect(() => {
+        let filteredContactList = userNameList.map((contact, index) => {
+            if(contact.includes(inputValue)){
+                return <div className={getContactClassName(index)} onClick={() => selectContact(index)} key={index}>{contact}</div>
+            }
+        });
+        setFilteredContacts(filteredContactList);
+    }, [inputValue, selectedName])
+
+    const getContactClassName = nameId => {
+        return selectedName === nameId ? `${styles['contact-name']} ${styles['select-contact']}` : `${styles['contact-name']}`;
+    }
+
+    const selectContact = nameId => {
+        setSelectedName(nameId);
+    }
+
     return(
         <div className={styles['filtered-contact-list']}>
             <div className={styles['filter-title']}>이름 일치 상위 항목</div>
             <div className={styles['contact-list']}>
-            { filteredContactList.length ? showContactList(filteredContactList) : 
+            { filteredContacts.length ? filteredContacts : 
                 <div className={styles['contact-notification']}>일치하는 항목이 없습니다.</div>}
             </div>
         </div>
