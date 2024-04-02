99021b4fd3e0cad9ef1f31bc086b1fda24935707
seohyunCho <shanny06@naver.com>
2024-04-02 12:02:05 +0900

    userInfoList 파일 분리 및 내용 수정

---
+++ src/components/contact/UserInfoList.js
@@ -0,0 +1,60 @@
+const userInfoList = [
+    {
+        profileImg: '/images/Contact/ProfileImg/haeun-profile-img.jpg',
+        name: '김하은',
+        phoneNumber: '010-5790-0633',
+        email: 'w2203@e-mirim.hs.kr',
+        address: '경북 구미시 도량동',
+        memo: '안녕하세요! 저는 프론트엔드 개발자 김하은입니다. 저희가 만든 작품이 즐거우셨다면 좋겠어요. 감상해주셔서 감사합니다!'
+    },
+    {
+        profileImg: '/images/Contact/ProfileImg/gayoon-profile-img.jpg',
+        name: '양가윤',
+        phoneNumber: '010-5046-8478',
+        email: 'w2207@e-mirim.hs.kr',
+        address: '광주광역시 북구',
+        memo: '안녕하세요. 오은영체제 프론트 개발자 양가윤입니다. 좋은 관람 하세요:)'
+    },
+    {
+        profileImg: '/images/Contact/ProfileImg/haeun-profile-img.jpg',
+        name: '이서영',
+        phoneNumber: '010-6412-9315',
+        email: 'w2210@e-mirim.hs.kr',
+        address: '서울특별시 동대문구',
+        memo: '안녕하세요~ 오은영체제 백엔드 개발자 이서영입니다! 해원이 뿡뿡'
+    },
+    {
+        profileImg: '/images/Contact/ProfileImg/haewon-profile-img.jpg',
+        name: '이해원',
+        phoneNumber: '010-5138-5250',
+        email: 'w2214@e-mirim.hs.kr',
+        address: '의성군 의성읍',
+        memo: '안녕하세요~ 저는 오은영체제 프론트 개발자 이해원입니당! 사진은 제 강아지 아기 봉구입니다~ 귀엽죠!!!!!!!!!!!!!'
+    },
+    {
+        profileImg: '/images/Contact/ProfileImg/seohyun-profile-img.jpg',
+        name: '조서현',
+        phoneNumber: '010-6220-1784',
+        email: 'w2215@e-mirim.hs.kr',
+        address: '서울특별시 관악구',
+        memo: '안녕하세요. 오은영체제 프론트엔드 개발자 조서현입니다! IT Show에 와주셔서 감사합니다. 즐거운 관람 되세요~!'
+    },
+    {
+        profileImg: '/images/Contact/ProfileImg/boram-profile-img.png',
+        name: '최보람',
+        phoneNumber: '010-9690-4784',
+        email: 'w2216@e-mirim.hs.kr',
+        address: '서울특별시 강서구',
+        memo: '안녕하세요. 여러분 저는 오은영체제 프론트엔드 개발을 맡은 최보람입니다. 재밌게 관람하시고 조심히 가세요~^^'
+    },
+    {
+        profileImg: '/images/Contact/ProfileImg/hyekyung-profile-img.png',
+        name: '황혜경',
+        phoneNumber: '010-5355-5711',
+        email: 'w2218@e-mirim.hs.kr',
+        address: '서울특별시 영등포구',
+        memo: '안녕하세요 여러분 저는 오은영체제의 백엔드 개발을 맡은 황혜경입니다. 즐거운 ITSHOW 관람 되시길^0^'
+    }
+]
+
+export default userInfoList;
