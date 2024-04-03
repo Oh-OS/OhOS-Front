38a7c82b2d9b8b729126a5f0d2d3cb924e4b4a47
rkdbs <w2207@e-mirim.hs.kr>
2024-04-03 15:33:33 +0900

    Feat: 입력한 텍스트 값 변경

--- src/components/translation/translation.js
+++ src/components/translation/translation.js
@@ -1,10 +1,48 @@
+import { useState } from 'react';
 import '../../styles/common/Style.css'
 import translationStyle from '../../styles/translation/translationPage.module.css'
 
 function Translation() {
-    return(
-        <div>
+    const [inputText, setInputText] = useState('');
+    const [resultText, setResultText] = useState('');
 
+    const handleButtonClick = () => {
+        setResultText(inputText);
+        setInputText(resultText);
+    }
+
+    return(
+        <div className={translationStyle['container']}>
+            <p className={translationStyle['title']}>번역</p>
+
+            <div className={translationStyle['box']}>
+                <div className={translationStyle['input']}>
+                    <input
+                        className={translationStyle['input-text']}
+                        placeholder='Enter text'
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
+                    />
+                </div>
+
+                <div className={translationStyle['change']}>
+                    <div className={translationStyle['change-img']}>
+                        <img
+                            src={process.env.PUBLIC_URL + '/images/change.png'}
+                            onClick={handleButtonClick}
+                        />
+                    </div>
+                </div>
+
+                <div className={translationStyle['result']}>
+                    <input
+                        className={translationStyle['result-text']}
+                        placeholder='텍스트 입력'
+                        value={resultText}
+                        onChange={(e) => setResultText(e.target.value)}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
