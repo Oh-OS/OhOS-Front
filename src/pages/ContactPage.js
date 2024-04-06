d7c0474267365f589f5b85bddc652ea9f859674f
seohyunCho <shanny06@naver.com>
2024-04-07 04:46:09 +0900

    Feat: title bar 추가

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,16 +1,18 @@
 import ContactList from "../components/contact/ContactList";
 import ContactInfo from "../components/contact/ContactInfo";
 import ContactProvider from "../components/contact/ContactProvider";
-
+import TitleBar from "../components/common/TitleBar";
 function ContactPage() {
     return(
-        <ContactProvider>
-            <div style={{display: "flex"}}>
-                <ContactList/>
-                <ContactInfo/>
-            </div>
-        </ContactProvider>
-        
+        <>
+            <TitleBar/>
+            <ContactProvider>
+                <div style={{display: "flex"}}>
+                    <ContactList/>
+                    <ContactInfo/>
+                </div>
+            </ContactProvider>
+        </>
     )
 }
 
