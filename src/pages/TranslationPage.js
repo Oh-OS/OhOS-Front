23c325b257c7f2febf11b5f97a52fff61fb47fa1
rkdbs <w2207@e-mirim.hs.kr>
2024-04-08 13:59:51 +0900

    Feat: 번역 title bar 추가

--- src/pages/TranslationPage.js
+++ src/pages/TranslationPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Translation from '../components/translation/translation';
+import TitleBar from "../components/common/TitleBar";
 
 function TranslationPage() {
     return (
-        <div>
+        <>
+            <TitleBar/>
             <Translation />
-        </div>
+        </>
     )
 }
 
