5e8eac3777dc14826985fb9687d1a9c45920a07e
boram065 <boram4784@gmail.com>
2024-04-09 11:26:43 +0900

    feat: 툴바 추가

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,13 +1,15 @@
 import '../../styles/common/Style.css'
 import weatherStyle from '../../styles/weather/Weather.module.css'
 
-import WeatherInfo from './WeatherInfo'
+import TitleBar from '../common/TitleBar';
+import WeatherInfo from './WeatherInfo';
 import WeatherSearch from './WeatherSearch';
 import WeatherShow from './WeatherShow';
 
 function Weather() {
     return(
         <div className={weatherStyle['container']}>
+            <div style={{position: "absolute", zIndex: 100}}> <TitleBar /> </div>
             <div className={weatherStyle['background']}>
                 <div className={weatherStyle['allDiv']}>
                     {/* 정보*/}
