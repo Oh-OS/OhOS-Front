7c7c76cda3cf761239e8a455285121e0cfd2e3de
seohyunCho <shanny06@naver.com>
2024-05-04 23:29:03 +0900

    Fix: main react-webcam으로 수정

--- src/components/photoBooth/MainCam.js
+++ src/components/photoBooth/MainCam.js
@@ -1,54 +1,25 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import '../../styles/common/Style.css'
 import style from '../../styles/photoBooth/MainCam.module.css'
+import Webcam from "react-webcam";
 
-function MainCam({ main }) {
-    const videoRef = useRef();
-    const canvasRef = useRef();
+function MainCam() {
+    const videoConstraints = {
+        width: 1280,
+        height: 720,
+        facingMode: "user"
+    };
+
+    const camAreaRef = useRef(null);
+    let camArea;
 
     useEffect(() => {
-        if(main){
-            console.log(main);
-            const video = videoRef.current;
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext('2d');
-    
-            // 웹캠에서 비디오 스트림 가져오기
-            navigator.mediaDevices.getUserMedia({ video: true })
-            .then(stream => {
-                video.srcObject = stream;
-                video.oncanplaythrough = function() {  // 로드 완료되면 실행
-                    video.play();
-                };
-            })
-            .catch(error => {
-                console.error('Error accessing webcam:', error);
-            });
-    
-            // 비디오 프레임을 Canvas에 그리기
-            const drawFrame = () => {
-                // Canvas에 좌우 반전 적용
-                ctx.save();
-                ctx.scale(-1, 1);
-                ctx.drawImage(video, -canvas.width, 0, canvas.width, canvas.height);
-                ctx.restore();
-                requestAnimationFrame(drawFrame);
-            };
-    
-            // 비디오가 로드되면 프레임 그리기 시작
-            video.addEventListener('loadeddata', drawFrame);
-    
-            return () => {
-                video.removeEventListener('loadeddata', drawFrame);
-            };
-        }
-    }, [main]);
-
+        camArea = camAreaRef.current;
+    }, [])
 
     return(
-        <div className={style['cam-area']}>
-            <video ref={videoRef} className={style['video']}></video>
-            <canvas ref={canvasRef} className={style['canvas']}></canvas>
+        <div className={style['cam-area']} ref={camAreaRef}>
+            <Webcam mirrored audio={false} width={1280} height={720} videoConstraints={videoConstraints}/>   
         </div>
     )
 }
