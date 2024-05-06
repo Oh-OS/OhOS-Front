8e8bc65fd781cd8fd2f5c2a3572adf212b0152ed
rlagkdms <ithaeun@naver.com>
2024-05-06 22:24:52 +0900

    fix : 검색결과창 이외의 부분 누르면 검색결과창 닫히게 하기

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import '../../styles/common/Style.css';
 import MapMenu from './MapMenu';
 import MapView from './MapView';
 
 function Map() {
+    const [isOpen , setIsOpen] = useState(false);
+    const handleResultBox = (open) => setIsOpen(!open); 
     return (
         <div style={{width: "100vw", height:"100vh", display: "flex"}}>
-        <MapMenu/>
-        <MapView/>
+        <MapMenu handleResultBox={handleResultBox} isOpen={isOpen}/>
+        <MapView handleResultBox={handleResultBox}/>
         </div>
     )
 }
