1fbd7e22231804a7bbc967670fd4a9ac76de48d1
rlagkdms <ithaeun@naver.com>
2024-05-07 10:21:11 +0900

    style : debounce input 클래스명 지정

--- src/components/map/MapMenu.js
+++ src/components/map/MapMenu.js
@@ -17,7 +17,7 @@ function MapMenu({handleResultBox, isOpen}){
 
             <div className={style['map-input']}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512" onClick={ () => setInputValue('')}><path fill="none" stroke="#828282" strokeMiterlimit="10" strokeWidth="32" d="M221.09 64a157.09 157.09 0 1 0 157.09 157.09A157.1 157.1 0 0 0 221.09 64Z"/><path fill="none" stroke="#828282" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32" d="M338.29 338.29L448 448" /></svg>
-                <DebounceInput placeholder='검색' value={ inputValue }  debounceTimeout = { 500 } onChange={ (e) => setInputValue(e.target.value) } onClick={()=>handleResultBox(isOpen)}/>
+                <DebounceInput className={style['debounce-input']} placeholder='검색' value={inputValue}  debounceTimeout={500} onChange={ (e) => setInputValue(e.target.value) } onClick={()=>handleResultBox(isOpen)}/>
             </div>
             <SearchResultComponent isOpen={isOpen}/>
             <FavoriteList imgUrl={ '/images/Map/heart.svg' } listTitle={ "즐겨찾기" }/>
