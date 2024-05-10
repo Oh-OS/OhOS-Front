1155eaa3a5e1c2977c3193356afd270dfa164a90
boram065 <boram4784@gmail.com>
2024-05-10 17:26:03 +0900

    Feat: 키워드로 목록 표출하기

--- src/components/map/MapMenu.js
+++ src/components/map/MapMenu.js
@@ -3,6 +3,7 @@ import style from '../../styles/map/Map.module.css';
 import { useState } from 'react';
 import { DebounceInput } from 'react-debounce-input';
 import { Icon } from '@iconify/react';
+import axios from 'axios';
 
 import SearchResultComponent from './SearchResultComponent'
 import RecentSearchList from './RecentSearchList';
@@ -12,17 +13,60 @@ import FavoriteList from './FavoriteList';
 /* 지도 메뉴 컴포넌트 */
 function MapMenu({handleResultBox, isOpen}){
     const [inputValue , setInputValue] = useState('');
+    const [searchList, setSearchList] = useState([]);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+        searchPlaces(e.target.value);
+    };
+
+    // Kakao Maps API를 사용하여 장소 검색
+    const searchPlaces = async () => {
+        const keyword = document.getElementById('keyword').value;
+        if (!keyword.trim()) {
+            setSearchList([]);
+            return;
+        }
+        const apiKey = '';
+        try {
+            const response = await axios.get(`https://dapi.kakao.com/v2/local/search/keyword.json?query=${keyword}`, {
+                headers: {
+                    'Authorization': `KakaoAK ${apiKey}`
+                }
+            });
+
+            if (response.status === 200) {
+                setSearchList(response.data.documents); // 검색 결과를 상태에 저장
+                console.log("검색 결과 저장 성공!");
+            } else {
+                console.log('검색 결과를 가져오는데 실패했습니다.');
+            }
+        } catch (error) {
+            console.error('검색 중 오류 발생:', error);
+        }
+    };
+
     return (
         <>
         <div className={style['map-menu-div']}>
 
             <div className={style['map-input']}>
                 <Icon icon='ion:search-outline' style={{ color: 'white' }}/>
-                <DebounceInput className={style['debounce-input']} placeholder='검색' value={inputValue}  debounceTimeout={500} onChange={ (e) => setInputValue(e.target.value) } onClick={()=>handleResultBox(isOpen)}/>
+                <DebounceInput 
+                    id='keyword' 
+                    className={style['debounce-input']} 
+                    placeholder='검색' value={inputValue} 
+                    debounceTimeout={500} 
+                    onChange={handleInputChange} 
+                    onClick={()=>handleResultBox(isOpen)}
+                />
             </div>
-            <SearchResultComponent isOpen={isOpen}/>
-            <FavoriteList listTitle={ "즐겨찾기" }/>
-            <RecentSearchList listTitle={ "최근 검색" }/>
+            {
+                searchList.length > 0 &&
+                <SearchResultComponent isOpen={isOpen} searchList={searchList}/>
+            }
+            <FavoriteList />
+            <RecentSearchList />
            
         </div>
 
--- src/components/map/RecentSearchList.js
+++ src/components/map/RecentSearchList.js
@@ -4,11 +4,11 @@ import RecentSearchItem from './RecentSearchItem';
 
 import { useState } from 'react';
 
-function RecentSearchList(props){
+function RecentSearchList(){
     const [recentList, setRecentList] = useState([{ title: "미림분식"},]);
     return (
         <div className={style['menu-list-div']}>
-            <div style={{color: "#A1A1A1", marginBottom:"7%", fontSize: 14}}>{props.listTitle}</div>
+            <div style={{color: "#A1A1A1", marginBottom:"7%", fontSize: 14}}>최근 검색</div>
             {
                 recentList.map(item => {
                   return  <RecentSearchItem itemTitle={item.title}/>
