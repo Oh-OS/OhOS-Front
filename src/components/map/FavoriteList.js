ac26ce886bab541736ed9ff47338890fe78db997
boram065 <boram4784@gmail.com>
2024-05-10 17:26:20 +0900

    Feat: 즐겨찾기 서버 연결

--- src/components/map/FavoriteList.js
+++ src/components/map/FavoriteList.js
@@ -1,21 +1,44 @@
+import axios from 'axios';
 import '../../styles/common/Style.css';
 import style from '../../styles/map/Map.module.css';
 import FavoriteItem from './FavoriteItem';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { MapHost } from '../../Config';
 
-function FavoriteList(props){
-    const [favoriteList, setFavoriteList] = useState([{ title: "미림마이스터고", favorite: false},]);
-    return (
-        <div className={style['menu-list-div']}>
-            <div style={{color: "#A1A1A1", marginBottom:"7%", fontSize: 14}}>{props.listTitle}</div>
-            {
-                favoriteList.map(item => {
-                  return  <FavoriteItem itemTitle={item.title}/>
-                })
+function FavoriteList(){
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`${MapHost}/bookmarks`);
+                if(response.status === 200) {
+                    console.log("데이터 불러오기 성공", response.data);
+                    setData(response.data);
+                } else {
+                    console.log("데이터 불러오기 실패: ", response.status);
+                }
+            } catch(error) {
+                console.log("서버 연결 실패 : ", error);
             }
-        </div>
-        
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            {
+                data.length > 0 &&
+                <div className={style['menu-list-div']}>
+                    <div style={{color: "#A1A1A1", marginBottom:"7%", fontSize: 14}}>즐겨찾기</div>
+                    {
+                        data.map(item => {
+                            return  <FavoriteItem title={item.locationName}/>
+                        })
+                    }
+                </div>
+            }
+        </>
     )
-} 
+}
 export default FavoriteList;
