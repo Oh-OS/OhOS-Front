f5abe798cd17b7d8405993f13a5f64563f9d41ef
boram065 <boram4784@gmail.com>
2024-05-12 19:20:34 +0900

    Rename: 파일 경로 변경

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,6 @@
 export const MapHost = 'http://localhost:8080';
 export const WeatherHost = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst';
 
-export const MapJSApiKey = '';
 export const MapRestApiKey = '';
 export const WeatherApiKey = '';
 export const TranslateApiKey = '';
