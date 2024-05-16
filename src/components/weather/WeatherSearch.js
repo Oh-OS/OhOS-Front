b5ab7bbde8f8b37f1317346fd118ea85db9a35bc
rkdbs <w2207@e-mirim.hs.kr>
2024-05-16 14:18:25 +0900

    Chore: 날씨 위도 경도 소수점 날리기

--- src/components/weather/WeatherSearch.js
+++ src/components/weather/WeatherSearch.js
@@ -33,7 +33,12 @@ function WeatherSearch() {
             });
 
             if (response.status === 200) {
-                setSearchList(response.data.documents);
+                const documents = response.data.documents.map(document => ({
+                    ...document,
+                    x: parseInt(document.x),
+                    y: parseInt(document.y)
+                }));
+                setSearchList(documents)
                 console.log("검색 결과 저장 성공!");
             } else {
                 console.log('검색 결과를 가져오는데 실패했습니다.');
