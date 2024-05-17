a402407369d247f24e066eaab32505603c21a333
boram065 <boram4784@gmail.com>
2024-05-17 14:11:52 +0900

    Fix: 즐겨찾기 추가하면 바로 화면에 표시하기

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -16,26 +16,36 @@ function Map() {
 
     // 지도 server api 연결
     const [data, setData] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get(`${MapHost}/bookmarks`);
-                if (response.status === 200) {
-                    console.log("데이터 불러오기 성공");
-                    setData(response.data);
-                } else {
-                    console.log("데이터 불러오기 실패 : ", response.status);
-                }
-            } catch(error) {
-                console.error("api 연결 실패 : ", error);
+    const [prevData, setPrevData] = useState([]);
+    async function fetchData() {
+        try {
+            const response = await axios.get(`${MapHost}/bookmarks`);
+            if (response.status === 200) {
+                console.log("데이터 불러오기 성공");
+                setData(response.data);
+            } else {
+                console.log("데이터 불러오기 실패 : ", response.status);
             }
+        } catch(error) {
+            console.error("api 연결 실패 : ", error);
         }
+    }
+
+    useEffect(() => {
         fetchData();
     }, []);
 
+    // 이전 데이터와 바뀐 데이터 비교하여 다르면 fetchData 호출
+    useEffect(() => {
+        if (JSON.stringify(data) !== JSON.stringify(prevData)) {
+            fetchData();
+            setPrevData(data);
+        }
+    }, [data]);
+
     return (
         <div style={{width: "100vw", height:"100vh", display: "flex"}}>
-            <MapMenu handleResultBox={handleResultBox} isOpen={isOpen} data={data} setLocation={setLocation}/>
+            <MapMenu handleResultBox={handleResultBox} isOpen={isOpen} data={data} setData={setData} setLocation={setLocation} />
             <MapView handleResultBox={handleResultBox} data={data} location={location} />
         </div>
     )
