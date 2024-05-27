6b80fc6991d6025646039aeb2e02f0c35074016b
해원 <haewon1106@gmail.com>
2024-05-27 09:55:11 +0900

    Docs: 시리 텍스트 화면 표시 구현

--- src/components/siri/Speech.py
+++ src/components/siri/Speech.py
@@ -7,9 +7,10 @@ from gtts import gTTS
 
 openai.api_key = ""
 
-def log_to_file(content, filename='conversation_logs.txt'):
-    with open(filename, 'a') as file:
+def log_to_file(content, filename='/Users/haewon/Desktop/project/OhOS/OhOS-Front/src/components/siri/conversation_logs.txt', mode='w'):
+    with open(filename, mode) as file:  # mode 인자 추가
         file.write(content + '\n')
+
 
 def text_to_voice(text):
     tts = gTTS(text=text, lang='ko')
@@ -67,6 +68,7 @@ while True:
             voice_recognition_started = True
             print("네?")
             text_to_voice("네?")
+            log_to_file("네?")
 
     if voice_recognition_started:
         prompt = voice_to_text(timeout=timeout_duration)
@@ -117,135 +119,3 @@ while True:
         time.sleep(0.1)
 
         content = content + msg
-
-# import openai
-# import time
-# import os
-# import playsound
-# import speech_recognition as sr
-# from gtts import gTTS
-# import requests
-
-# openai.api_key = 
-
-# def log_to_file(content, filename='conversation_logs.txt'):
-#     with open(filename, 'a') as file:
-#         file.write(content + '\n')
-
-# def text_to_voice(text):
-#     tts = gTTS(text=text, lang='ko')
-#     filename = 'voice3.mp3'
-#     tts.save(filename)
-#     playsound.playsound(filename)
-#     os.remove(filename)
-
-# def voice_to_text(timeout=None):
-#     r = sr.Recognizer()
-#     with sr.Microphone() as source:
-#         print("말해주세요: ")
-#         audio = r.listen(source)
-#         said = " "
-
-#         try:
-#             said = r.recognize_google(audio, language="ko-KR")
-#             print("당신의 말: ", said)
-#         except:
-#             pass
-#             time.sleep(3)
-
-#     return said
-
-# file_path = 'conversation_logs.txt'
-
-# with open(file_path, 'w') as f:
-#     f.write('')
-
-# if os.path.exists(file_path):
-#     print(f"{file_path} 파일이 성공적으로 생성되었습니다.")
-# else:
-#     print(f"{file_path} 파일이 생성되지 않았습니다.")
-
-# content = ''
-# voice_recognition_started = False
-# timeout_duration = 5
-
-# pages = {
-#     '지도': '/map',
-#     '번역': '/translation',
-#     '연락처': '/contacts',
-#     '버블': '/bubble',
-#     '날씨': '/weather',
-#     '비스코드': 'https://github.dev/github/dev',
-#     '비주얼스튜디오코드': 'https://github.dev/github/dev',
-#     '브이에스코드': 'https://github.dev/github/dev',
-#     '사파리': 'https://www.google.co.kr'
-# }
-
-# # Node.js 서버의 URL
-# url = 'http://localhost:3000/conversation_logs'
-
-# while True:
-#     if not voice_recognition_started:
-#         prompt = voice_to_text()
-#         if '시리야' in prompt:
-#             voice_recognition_started = True
-#             print("네?")
-#             text_to_voice("네?")
-
-#     if voice_recognition_started:
-#         prompt = voice_to_text(timeout=timeout_duration)
-#         if not prompt:
-#             break
-
-#         command = next((key for key in pages.keys() if key in prompt), None)
-#         action_keywords = ["열어", "켜", "틀어", "오픈", "보여", "이동", "옮겨"]
-
-#         if command and any(word in prompt for word in action_keywords):
-#             page = pages[command]
-#             content = page
-#             continue
-
-#         try:
-#             messages = [
-#                 {'role': 'system', 'content': 'You are a helpful assistant.'},
-#                 {'role': 'user', 'content': content},
-#             ]
-#             messages.append({'role': 'assistant', 'content': 'You are a helpful assistant.' })
-#             messages.append({'role': 'user', 'content': prompt })
-#         except:
-#             messages = [
-#                 {'role': 'system', 'content': 'You are a helpful assistant.'},
-#                 {'role': 'user', 'content': prompt},
-#             ]
-
-#         if '굿바이' in prompt:
-#             text_to_voice("시리를 종료합니다.")
-#             print("시리를 종료합니다.")
-#             log_to_file("시리를 종료합니다.")
-#             break
-
-#         response = openai.ChatCompletion.create(
-#             model='gpt-3.5-turbo',
-#             messages=messages
-#         )
-
-#         print("--------------------------------")
-#         print(str(response['choices'][0]['message']['content']).strip())
-#         msg = str(response['choices'][0]['message']['content']).strip()
-#         print("--------------------------------")
-
-#         log_to_file(msg)
-
-#         text_to_voice(msg)
-
-#         time.sleep(0.1)
-
-#         content = content + msg
-
-#     response = requests.get(url)
-
-#     if response.status_code == 200:
-#         data = response.text
-#         print(data)
-#     else:
-#         print('에러 : ', response.status_code)
