f559e2481b61c843260e7690b79f9f4516113c35
seohyunCho <shanny06@naver.com>
2024-05-28 01:05:48 +0900

    Comment: 주석 제거

--- src/components/photoBooth/filters/StretchH.js
+++ src/components/photoBooth/filters/StretchH.js
@@ -10,16 +10,4 @@ export default function StretchH(ctx, video, canvas, width, height, drawImge) {
     // 이미지 객체, 이미지 내 x좌표, 이미지 내 y좌표, 이미지 넓이, 이미지 높이, 캔버스 x좌표, 캔버스 y좌표, 캔버스 넓이, 캔버스 높이
 
     setTimeout(drawImge, 33);
-
-    // 오른쪽 이미지 그리기
-    // ctx.translate(canvas.width, 0);
-    // ctx.scale(-1, 1);
-    // ctx.drawImage(video.video, 0, 0, width / 2, height, 0, 0, width / 2, height);
-    // ctx.save();
-
-    // // 왼쪽 이미지 그리기
-    // ctx.translate(canvas.width, 0);
-    // ctx.scale(-1, 1);
-    // ctx.drawImage(video.video, 0, 0, width / 2, height, 0, 0, width / 2, height);
-    // setTimeout(drawImge, 33);
 };
