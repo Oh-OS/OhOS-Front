84bda2256422e60585afa9180df001415f033cc4
rkdbs <w2207@e-mirim.hs.kr>
2024-05-27 10:35:26 +0900

    Style: div 추가

--- src/components/weather/WeatherSearchList.js
+++ src/components/weather/WeatherSearchList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import WeatherSearchListStyle from '../../styles/weather/WeatherSearchList.module.css';
 
-function WeatherSearchList({ searchList }) {
+function WeatherSearchList({ searchList, onItemClick }) {
     return (
         <div className={WeatherSearchListStyle['resultsListDiv']}>
             {searchList.map((item, index) => (
                 <div className={WeatherSearchListStyle['resultsListContainer']}>
-                    <div key={index} className={WeatherSearchListStyle['resultsListBox']}>
+                    <div 
+                        key={index} 
+                        className={WeatherSearchListStyle['resultsListBox']}
+                        onClick={() => onItemClick(item)}
+                    >
                         <p>{item.address_name}</p>
                     </div>
                 </div>
