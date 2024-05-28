07e66ad86954dc388e64aeb96f6c4a812ca92cb5
boram065 <boram4784@gmail.com>
2024-05-29 08:14:15 +0900

    Remove: 파일 삭제

--- src/Config.js
+++
@@ -1,6 +0,0 @@
-export const MapHost = 'http://localhost:8080';
-export const WeatherHost = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst';
-
-export const MapRestApiKey = '';
-export const WeatherApiKey = '';
-export const TranslateApiKey = '';
