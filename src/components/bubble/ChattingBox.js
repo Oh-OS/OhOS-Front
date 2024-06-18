401148aceda4f152e5478daae1a6fea937f140c7
해원 <haewon1106@gmail.com>
2024-06-18 22:35:28 +0900

    Docs: 마지막 채팅 불러오기

--- src/components/bubble/ChattingBox.js
+++ src/components/bubble/ChattingBox.js
@@ -4,9 +4,6 @@ import MyChat from './MyChat';
 import YourChat from './YourChat';
 
 function ChattingBox({ message }) {
-    // console.log("메시지",message)
-
-    console.log("잘되나유")
     const renderChat = () => {
         if (message.userKey === 8) {
             return <MyChat text={message.chat} />;
--- src/components/bubble/UserBox.js
+++ src/components/bubble/UserBox.js
@@ -1,19 +1,28 @@
 import React, {useContext} from 'react';
 import '../../styles/common/Style.css'
 import style from '../../styles/bubble/UserBox.module.css'
+import axios from 'axios';
 
 import { BubbleContext } from './BubbleProvider';
 import userInfoList from'./UserInfoList';
 
-
-function UserBox({ name, onClick, handleRoomClick, room }) {
+function UserBox({ name, onClick, handleRoomClick, room, lastChat, createdAt }) {
     const { getBubbleClassName, setSelectedName, getUserline } = useContext(BubbleContext);
 
     const handleClick = () => {
         handleRoomClick(room);
         setSelectedName(name);
     };
-        
+
+    const formatDate = (date) => {
+        if (date) {
+            const formattedDate = new Date(date).toLocaleDateString('en-CA', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/-/g, '/');
+            return formattedDate;
+        } else {
+            return '';
+        }
+    }
+
     return(
         <>
             <div className={getBubbleClassName(name)} onClick={handleClick}>
@@ -21,10 +30,10 @@ function UserBox({ name, onClick, handleRoomClick, room }) {
                 <div className={style['chat-info']}>
                     <div className={style['user-info']}>
                         <div className={style['user-name']}>{name}</div>
-                        <div className={style['recent-date']}>2024/3/11</div>
+                        <div className={style['recent-date']}>{formatDate(createdAt)}</div>
                     </div>
                     <div className={style['chat-detail']}>
-                        안영랗세요ㅗㅇ 안녀아헷ㅇ 아니에ㅑ노라ㅓㄴ라도절더ㅏㅗ
+                        {lastChat}
                     </div>
                 </div>
             </div>
